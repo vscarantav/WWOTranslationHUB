df2707c110f614444e9d2485a1d16c0ecc13dbb7
Vinicius Tavares <vscaran@byui.edu>
2026-08-12 14:17:37 -0600

    Add instructure.com/assessment_questions/ to ignored domains list to prevent prompting for quiz asset files

--- app/controller.py
+++ app/controller.py
@@ -264,7 +264,7 @@ class TranslationController:
             clean_url = unescaped_url
             
             # Ignore standard XML namespaces and Canvas backend links
-            ignore_domains = ['w3.org', 'purl.org', 'imsglobal.org', 'canvas.instructure.com', 'ieee.org', 'instructure.com/api/', 'byui-lti-to-url.azurewebsites.net', 'googleusercontent.com']
+            ignore_domains = ['w3.org', 'purl.org', 'imsglobal.org', 'canvas.instructure.com', 'ieee.org', 'instructure.com/api/', 'byui-lti-to-url.azurewebsites.net', 'googleusercontent.com', 'instructure.com/assessment_questions/']
             if any(domain in clean_url for domain in ignore_domains):
                 return url
             
